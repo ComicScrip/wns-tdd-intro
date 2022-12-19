b28be062891927b8c9e68de806afa5a01ca49d01
Pierre Genthon <genthon.pierre@gmail.com>
2022-12-19 14:57:28 +0100

    lc

--- toCelsius.ts
+++ toCelsius.ts
@@ -1,9 +1,9 @@
-const getPrecision = (n: number, precision: number) =>
+const withPrecision = (n: number, precision: number) =>
   Number(n.toFixed(precision));
 
-export function toCelsius(n?: any, precision = 2, unit: "k" | "f" = "f") {
-  const number = parseFloat(n);
-  if (isNaN(number)) return null;
-  if (unit === "k") return getPrecision(n - 273.15, precision);
-  return getPrecision(((number - 32) * 5) / 9, precision);
+export function toCelsius(input?: any, precision = 2, unit: "k" | "f" = "f") {
+  const num = parseFloat(input);
+  if (isNaN(num)) return null;
+  if (unit === "k") return withPrecision(num - 273.15, precision);
+  return withPrecision((num - 32) * (5 / 9), precision);
 }
